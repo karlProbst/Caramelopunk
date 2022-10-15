a0ed300c27d4e8260db7f49ccdd8354de2a178db
MaywormIII <gilbertomaywormneto@gmail.com>
2022-10-15 17:22:59 -0300

    tile shfler prttp n resolution fix

--- RunnerScene/enemy_spawn.gd
+++ RunnerScene/enemy_spawn.gd
@@ -1,21 +1,30 @@
 extends Node
 
+onready var front_tile_01 = $FrontTile_01
+onready var front_tile_02 = $FrontTile_02
+onready var front_tile_03 = $FrontTile_03
+onready var front_tile_04 = $FrontTile_04
+onready var front_tile_05 = $FrontTile_05
 
-enum {
-	IDLE, RUN, BARK
-	}
+var vel := 5.0
 
-var state := RUN
+var rng = RandomNumberGenerator.new()
 
-# Called when the node enters the scene tree for the first time.
+onready var tiles_array = [front_tile_01,front_tile_02,front_tile_03,front_tile_04,front_tile_05]
+
 func _ready():
 	pass
 
 func _process(delta):
-	match state:
-		IDLE:
-			print_debug("idle")
-		RUN:
-			print_debug("run")
-		BARK:
-			print_debug("bark")
+	
+	for tile in tiles_array:
+		tile.position.x -= vel
+		tile.frame = rng.randi_range(0,3)
+		if tile.position.x < 0:
+			tile.position.x = 2400
+
+func front_tile_controller():
+	pass
+
+func back_tile_controller():
+	pass
