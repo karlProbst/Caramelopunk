2e3d58bfcb79b12c3147f8c73ff9dc886eba4113
MaywormIII <gilbertomaywormneto@gmail.com>
2022-10-15 22:41:05 -0300

    added obstacles on runner

--- RunnerScene/enemy_spawn.gd
+++ RunnerScene/enemy_spawn.gd
@@ -6,9 +6,15 @@ onready var front_tile_03 = $FrontTile_03
 onready var front_tile_04 = $FrontTile_04
 onready var front_tile_05 = $FrontTile_05
 
+onready var obstacle_instance = load("res://RunnerScene/Obstacles/Obstacle.tscn")
+
 var vel := 10.0
 
+var obstacle_spawn_timer = 0
+
+
 var rng = RandomNumberGenerator.new()
+
 
 onready var tiles_array = [front_tile_01,front_tile_02,front_tile_03,front_tile_04,front_tile_05]
 
@@ -17,7 +23,6 @@ func _ready():
 		tile.frame = rng.randi_range(0,3)
 
 func _process(delta):
-	
 	front_tile_controller()
 
 func front_tile_controller():
@@ -28,9 +33,17 @@ func front_tile_controller():
 			
 			tile.frame = rng.randi_range(0,3)
 			tile.position.x = 2400
+			obstacle_spawn_timer += 1
+			if obstacle_spawn_timer >= 5:
+				obstacle_spawner()
+			print(obstacle_spawn_timer)
 
 func back_tile_controller():
 	pass
 
 func obstacle_spawner():
-	pass
+	var obstacle = obstacle_instance.instance()
+	obstacle.position = Vector2(2400,666)
+	self.add_child(obstacle)
+	print(obstacle)
+	obstacle_spawn_timer = 0
