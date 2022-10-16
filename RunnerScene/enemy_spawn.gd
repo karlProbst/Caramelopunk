c082e95a120453e4e868cf1036fc1d89c60bb303
MaywormIII <gilbertomaywormneto@gmail.com>
2022-10-15 23:20:39 -0300

    coin spawner logic

--- RunnerScene/enemy_spawn.gd
+++ RunnerScene/enemy_spawn.gd
@@ -1,5 +1,8 @@
 extends Node
 
+#This code has the spawn for tiles, obstacles and coins.
+#didn't change the name. was afreaid it would break ;(
+
 onready var front_tile_01 = $FrontTile_01
 onready var front_tile_02 = $FrontTile_02
 onready var front_tile_03 = $FrontTile_03
@@ -10,7 +13,8 @@ onready var obstacle_instance = load("res://RunnerScene/Obstacles/Obstacle.tscn"
 
 var vel := 10.0
 
-var obstacle_spawn_timer = 0
+var obstacle_spawn_timer = 0 #Counter based on the tiles to spawn obstacles... including enemies
+var coin_spawn_timer = 0
 
 
 var rng = RandomNumberGenerator.new()
@@ -34,11 +38,17 @@ func front_tile_controller():
 			tile.frame = rng.randi_range(0,3)
 			tile.position.x = 2400
 			obstacle_spawn_timer += 1
+			coin_spawn_timer += 1
 			if obstacle_spawn_timer >= 5:
 				obstacle_spawner()
+			if obstacle_spawn_timer >= 6:
+				coin_spawner()
 			print(obstacle_spawn_timer)
 
 func back_tile_controller():
+	pass
+
+func coin_spawner():
 	pass
 
 func obstacle_spawner():
